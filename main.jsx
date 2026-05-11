b6210bc604e79875dae8b274f5e4cfd4b44ae727
Akira Takasugi <hardminimal_nekomi@outlook.com>
2026-05-11 14:35:51 +0900

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import HotlineAIAgentV3 from './hotline_ai_agent_v3.jsx';
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HotlineAIAgentV3 />
-  </React.StrictMode>
-);
